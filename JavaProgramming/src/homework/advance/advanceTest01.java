f0a35ef9efb96dcac498a9ad4c04d92e12590644
jangho309 <jangho309@naver.com>
2024-04-29 21:41:21 +0900

    homework upload

---
+++ JavaProgramming/src/homework/advance/advanceTest01.java
@@ -0,0 +1,31 @@
+package homework.advance;
+
+import java.util.Scanner;
+
+public class advanceTest01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] moneyUnit = {50000, 10000, 5000, 1000, 500, 100, 50, 10, 1};
+		int[] answerUnit = {0, 0, 0, 0, 0, 0, 0, 0 , 0};
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("금액을 입력해 주세요.");
+		int calculateBill = sc.nextInt();
+		int minusBill = calculateBill;
+		for(int i = 0; i < moneyUnit.length; i++) {
+			answerUnit[i] = minusBill / moneyUnit[i];
+			minusBill = minusBill - (moneyUnit[i] * answerUnit[i]);
+		}
+		
+		for(int i = 0; i < answerUnit.length; i++) {
+			if(moneyUnit[i] > 1000) {
+				System.out.println(moneyUnit[i] + "원 : " + answerUnit[i] + "매");
+			} else {
+				System.out.println(moneyUnit[i] + "원 : " + answerUnit[i] + "개");
+			}
+		}
+		sc.close();
+	}
+
+}
---
+++ JavaProgramming/src/homework/basic/basicTest11.java
@@ -0,0 +1,20 @@
+package homework.basic;
+
+public class basicTest11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] ranArr = new int[10];
+		for(int i = 0; i < ranArr.length; i++) {
+			ranArr[i] = (int)(Math.random() * 10 + 1);
+			
+		}
+		
+		for(int j = 0; j < ranArr.length; j++) {
+			if(ranArr[j] % 2 == 1) {
+				System.out.println(ranArr[j]);
+			}
+		}
+	}
+
+}
